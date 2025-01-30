4d29312bf20568c66202d4048f45a90d8e17e866
= <=>
2025-01-30 14:20:46 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import CartContextProvider from "./context/CartItems.Context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/home">
+    <BrowserRouter basename="/https://bharat-go-upce.vercel.app/home/">
       <CartContextProvider>
         <App />
       </CartContextProvider>
